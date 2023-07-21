5c64ebe6d969f407d7ab5cba4ab9d1cc4e72817c
John Lucas <joohnzay@gmail.com>
2023-07-21 16:28:03 -0300

    delete, update and finish

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -17,7 +17,23 @@ const createTask = async (task) => {
     return {insertId: createdTask.insertId};
 };
 
+const deleteTask = async (id) => {
+    const [removedTask] = await connection.execute('DELETE FROM tasks WHERE id = ?', [id])
+    return removedTask;
+};
+
+const updateTask = async (id, task) => {
+    const query = 'UPDATE tasks SET title = ?, status = ? WHERE id = ?';
+
+    const {title, status} = task;
+
+    const [updatedTask] = await connection.execute(query, [title, status, id])
+    return updatedTask;
+};
+
 module.exports = {
     getAll,
-    createTask
+    createTask,
+    deleteTask,
+    updateTask
 };
