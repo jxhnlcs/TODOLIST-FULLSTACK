1bed75f0180c13849ee52f064c9e832d3522ae0e
John Lucas <joohnzay@gmail.com>
2023-07-21 11:40:50 -0300

    controller funcionando
    testado no insomnia

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/tasks')
+const tasksController = require('./controllers/tasksController');
+
+router.get('/tasks', tasksController.getAll)
 
 module.exports = router;
